3af6d10f9827a73e9705fb0df86e468a43c9e832
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-21 20:52:09 +0530

    intro pages

---
+++ grocery_app/lib/pages/intro_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          //logo
+
+
+          //we deliver groceries to doorstep
+
+
+          //fresh items everyday
+
+          //get started button
+
+
+        ],)
+    );
+  }
+}
