9a60f37e07826b8452c9859f9620b7b61dc8cddf
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-21 20:58:45 +0530

    intro page logo and text with paddings

--- grocery_app/lib/pages/intro_page.dart
+++ grocery_app/lib/pages/intro_page.dart
@@ -9,10 +9,16 @@ class IntroPage extends StatelessWidget {
       body: Column(
         children: [
           //logo
-
+          Padding(
+            padding: const EdgeInsets.only(left:80.0,right:80.0, bottom: 80.0,top:120),
+            child: Image.asset('lib/images/avocado.png'),
+          ),
 
           //we deliver groceries to doorstep
-
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Text("We deliver groceries to your doorstep", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+          ),
 
           //fresh items everyday
 
