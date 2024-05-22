7783449698bc6c7178e1ad62f35eee2fc1dd35e6
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-22 19:02:50 +0530

    home page greeting msg with paddings

--- grocery_app/lib/pages/home_page.dart
+++ grocery_app/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,15 +7,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column (
-        children: [
-          //good morning
-
-
-
-          //Let's order fresh items for you
-
-        ],)
+      body: SafeArea(
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 48), 
+            //good morning
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text("Good Morning,"),
+              ),
+        
+        const SizedBox(height: 4),
+            //Let's order fresh items for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text("Let's order fresh items for you", style: GoogleFonts.notoSerif(fontSize: 36,fontWeight: FontWeight.bold,),),
+          ),
+        
+            //divider
+        
+        
+            //fresh items + grid
+        
+        
+        
+          ],),
+      ),
     );
   }
 }
