5ba456f88c3e660ac8b43d9ae724c00532846621
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-05-24 20:16:41 +0530

    provider

--- grocery_app/lib/pages/home_page.dart
+++ grocery_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/components/gorcery_item_tile.dart';
+import 'package:grocery_app/models/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -50,12 +52,23 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Expanded(
-                child: GridView.builder(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return const GroceryItemTile();
-                }),)
+              child: Consumer<CartModel>(builder: (context, value, child){
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      color: value.shopItems[index][3],
+
+
+                    );
+                  });
+              }),
+
+            )
           ],
         ),
       ),
